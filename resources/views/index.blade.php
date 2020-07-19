fffadf024a84c3c67f6ba6b6ebbc3a24f4dda67a
jivadio <malomartin@protonmail.com>
2020-07-19 18:46:51 +0200

    Add Text for news and footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,41 @@
         <div class="row">
             <div class="col-md-8 bg-dark text-center">
                 <h4 style="color: white;">Dernières Annonces</h4>
-            </div>
+                <!--Carousel Wrapper-->
+<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+      <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+      <!--First slide-->
+      <div class="carousel-item active">
+        <h3 style="color: white;">Session de recrutement Juillet 2020</h3>
+        <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed tellus at velit tempus pretium nec vel neque. Integer justo leo, tincidunt eu malesuada in, mattis sed orci. Pellentesque vitae vestibulum est, quis pulvinar eros. Phasellus ac felis vel risus gravida aliquam sed vitae sapien. In egestas ante urna, vel euismod justo elementum ac. Ut semper facilisis accumsan. Duis metus risus, porttitor id sem vitae, dapibus malesuada sapien. Etiam sagittis metus ac urna vulputate interdum. Aliquam interdum sagittis nisl eu venenatis. Suspendisse a elementum erat. Sed tempus euismod ex, ut pharetra urna condimentum ac. Proin vulputate risus at blandit sollicitudin.</p>
+      </div>
+      <!--/First slide-->
+      <!--Second slide-->
+      <div class="carousel-item">
+        <h3 style="color: white;">Changement de direction</h3>
+        <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed tellus at velit tempus pretium nec vel neque. Integer justo leo, tincidunt eu malesuada in, mattis sed orci. Pellentesque vitae vestibulum est, quis pulvinar eros. Phasellus ac felis vel risus gravida aliquam sed vitae sapien. In egestas ante urna, vel euismod justo elementum ac. Ut semper facilisis accumsan. Duis metus risus, porttitor id sem vitae, dapibus malesuada sapien. Etiam sagittis metus ac urna vulputate interdum. Aliquam interdum sagittis nisl eu venenatis. Suspendisse a elementum erat. Sed tempus euismod ex, ut pharetra urna condimentum ac. Proin vulputate risus at blandit sollicitudin.</p>
+      </div>
+      <!--/Second slide-->
+      <!--Third slide-->
+      <div class="carousel-item">
+        <h3 style="color: white;">Frère j'ai pas d'idée</h3>
+        <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed tellus at velit tempus pretium nec vel neque. Integer justo leo, tincidunt eu malesuada in, mattis sed orci. Pellentesque vitae vestibulum est, quis pulvinar eros. Phasellus ac felis vel risus gravida aliquam sed vitae sapien. In egestas ante urna, vel euismod justo elementum ac. Ut semper facilisis accumsan. Duis metus risus, porttitor id sem vitae, dapibus malesuada sapien. Etiam sagittis metus ac urna vulputate interdum. Aliquam interdum sagittis nisl eu venenatis. Suspendisse a elementum erat. Sed tempus euismod ex, ut pharetra urna condimentum ac. Proin vulputate risus at blandit sollicitudin.</p>
+      </div>
+      <!--/Third slide-->
+    </div>
+    <br>
+    <!--/.Slides-->
+  </div>
+  <!--/.Carousel Wrapper-->
+        </div>
             <div class="col-md-4 blue-gradient text-center">
                 <h4 style="color: white;">Avis de Recherche</h4>
                 <br>
@@ -59,5 +93,13 @@
                   <br>
             </div>
         </div>
+    </div>
+    <br>
+    <div class='w-100 blue-gradient fixed-bottom'>
+        <p class='text-light text-center p-1 m-0'>
+            &copy; - 2020 | 
+            Version de développement | 
+            00/00/0000
+        </p>
     </div>
 @endsection
