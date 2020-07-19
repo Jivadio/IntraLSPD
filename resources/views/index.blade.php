e4dbef01b9980479de6dd005d6634fdb4955410a
jivadio <malomartin@protonmail.com>
2020-07-19 18:28:54 +0200

    Add Bar with Dagerous Level

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,11 +20,31 @@
                         <img src="{!! asset(config('path.images')).'/suspect-1.jpg' !!}" style="width: 75px; height: 75px; border-radius: 150%;">
                         <h5 style="color: white;">Sara Lopez-Aranda</h5>
                         <p style="color: white;">Viol - Délit de Fuite</p>
+                        <br>
+                        <h6 style="color: white;">Individu Très Dangereux</h6>
+                        <div class="progress">
+                            <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                       </div>
                       <div class="carousel-item">
                         <img src="{!! asset(config('path.images')).'/suspect-2.jpg' !!}" style="width: 75px; height: 75px; border-radius: 150%;">
                         <h5 style="color: white;">Karim Zenoud</h5>
                         <p style="color: white;">Délit de fuite</p>
+                        <br>
+                        <h6 style="color: white;">Individu Calme</h6>
+                        <div class="progress">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="{!! asset(config('path.images')).'/suspect-3.jpg' !!}" style="width: 75px; height: 75px; border-radius: 150%;">
+                        <h5 style="color: white;">Charles Fauchaux</h5>
+                        <p style="color: white;">Excès de Vitesse Récidiviste</p>
+                        <br>
+                        <h6 style="color: white;">Individu Dangereux</h6>
+                        <div class="progress">
+                            <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                       </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
