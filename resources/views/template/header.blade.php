423f1df20f7a01b3971c7934c57ec4f67c1ecbf6
jivadio <malomartin@protonmail.com>
2020-07-19 18:32:29 +0200

    minor fix

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -4,7 +4,7 @@
             <div class="col-12 d-flex align-items-center justify-content-between">
                 <div class="d-flex align-items-center">
                     <img src="{!! asset(config('path.images')).'/lspd-logo.png' !!}" style="width: 150px; height=150px;">
-                    <h1 style="color: white;">Los Santos Police Department</h1>
+                    <h1 style="color: white;">Los Santos Police Departement</h1>
                 </div>
                 <div class="btn-group" role="group" aria-label="Basic example">
                     <button type="button" class="btn btn-indigo btn-sm"><i class="far fa-user fa-sm pr-2" aria-hidden="true"></i>Connexion</button>
